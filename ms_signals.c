7952d19ee2f58206f5c38396444c36ff24972e59
Walana <robin.ficht@free.fr>
2023-05-05 14:53:41 +0200

    added trimHashtag

--- ms_signals.c
+++ ms_signals.c
@@ -6,7 +6,7 @@
 /*   By: rficht <robin.ficht@free.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 13:12:35 by rficht            #+#    #+#             */
-/*   Updated: 2023/05/05 10:58:48 by rficht           ###   ########.fr       */
+/*   Updated: 2023/05/05 14:43:43 by rficht           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ static void	ft_sighandle(int sig, siginfo_t *info, void *context)
 	if (sig == SIGINT)
 	{
 		printf("received a SIGINT (ctrl + C)\n");
-		rl_on_new_line();
 	}
 }
 
