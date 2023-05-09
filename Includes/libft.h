7d39baae0aa67c5d012bb5b8228d397a5534340b
Walana <robin.ficht@free.fr>
2023-05-09 16:53:59 +0200

    creation of ft_trunc

--- Includes/libft.h
+++ Includes/libft.h
@@ -73,6 +73,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strtrim(char const *s1, char const *set);
+int		ft_strtrunc(char *str, int start, int len);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
---
+++ libft/ft_strtrunc.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrunc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rficht <robin.ficht@free.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 10:17:09 by rficht            #+#    #+#             */
+/*   Updated: 2023/05/09 14:52:39 by rficht           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Includes/libft.h"
+#include <stdio.h>
+
+static int	is_valid(char *str, int start, int len)
+{
+	int	n;
+
+	n = 0;
+	while (str[n] && n < start + len)
+		n++;
+	if (n == start + len)
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_strtrunc(char *str, int start, int len)
+{
+	int		n;
+	char	*result;
+	char	temp;
+
+	if (!str)
+		return (1);
+	n = start;
+	if (!is_valid(str, start, len))
+		return (1);
+	while (n < start + len)
+	{
+		str[n] = str[n + len];
+		if (str[n] == 0)
+			break ;
+		n++;
+	}
+	str[n] = 0;
+	result = ft_strdup(str);
+	free(str);
+	str = result;
+	return (0);
+}
