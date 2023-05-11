3bcf4ab0263be0173ae2d0488186aeb0e1cc9c1c
Walana <robin.ficht@free.fr>
2023-05-11 11:48:55 +0200

    created freeing functions

--- Includes/libft.h
+++ Includes/libft.h
@@ -80,6 +80,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+void	ft_freenull(void **ptr);
+void	ft_freestr(char **ptr);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
