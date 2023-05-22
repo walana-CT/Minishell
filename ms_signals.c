417825172cf6d13fb974378fcafca0328151abb2
Walana <robin.ficht@free.fr>
2023-05-22 16:26:41 +0200

    ctrl + \ is deactivated

--- ms_signals.c
+++ ms_signals.c
@@ -6,7 +6,7 @@
 /*   By: rficht <robin.ficht@free.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 13:12:35 by rficht            #+#    #+#             */
-/*   Updated: 2023/05/19 15:52:17 by rficht           ###   ########.fr       */
+/*   Updated: 2023/05/22 16:26:10 by rficht           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,22 @@ static void	ft_sighandle(int sig, siginfo_t *info, void *context)
 	if (sig == SIGINT)
 	{
 		write(1, "\n", 1);
-		//rl_on_new_line();
 		rl_replace_line("", 0);
 		if (stat_interactive(-1))
-		{
-			//printf("ctrl + c called in non interactive mode\n");
-			//write(1, "\n", 1);
-			rl_on_new_line();			
-		}
+			rl_on_new_line();
 		rl_redisplay();
-
 	}
 }
 
 void	set_sig(void)
 {
 	struct sigaction	sa;
-	
+
 	sa.sa_flags = SA_RESTART;
 	sa.sa_sigaction = ft_sighandle;
-	//sa.sa_mask = SIGKILL;
+
 	if (sigaction(SIGINT, &sa, NULL) == -1)
+		ms_crash(NULL);
+	if (sigaction(SIGQUIT, &sa, NULL) == -1)
 		ms_crash(NULL);
 }
