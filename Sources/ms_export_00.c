4ceac2fe678a9f29b806ce4deb09011192843822
Walana <robin.ficht@free.fr>
2023-06-12 12:05:20 +0200

    started implmenting += in export

--- Sources/ms_export_00.c
+++ Sources/ms_export_00.c
@@ -6,7 +6,7 @@
 /*   By: rficht <robin.ficht@free.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 11:02:48 by rficht            #+#    #+#             */
-/*   Updated: 2023/06/11 09:23:28 by rficht           ###   ########.fr       */
+/*   Updated: 2023/06/12 11:52:39 by rficht           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	env_addvar(char *new_var, t_ms *ms)
 	char	**new_envp;
 	int		n;
 
+	printf("envreplace called\n");
 	n = -1;
 	new_envp = calloc(ms_sizeof_tab(ms->envp) + 2, sizeof(char *));
 	if (!new_envp)
@@ -30,18 +31,63 @@ void	env_addvar(char *new_var, t_ms *ms)
 
 int	valid_var(char *new_var)
 {
-	if (*new_var == '=')
+	if (*new_var == '=' || *new_var == '+')
 		return (FALSE);
 	while (*new_var && *new_var != '=')
 	{
 		if (*new_var == '-')
 			return (FALSE);
-		if (ft_isdigit(*new_var))
+		else if (*new_var == '+')
+		{
+			if (*(new_var + 1) == '=')
+				return (TRUE);
+			else
+				return (FALSE);
+		}
+		else if (!ft_isalnum(*new_var) && *new_var != '_')
 			return (FALSE);
 		new_var++;
 	}
 	return (TRUE);
 }
+
+
+
+/**
+ * test wether its an export += or not
+ * @param new_var the new variable to export.
+ * @return boolean.
+ */
+int	is_envinsert(char *new_var)
+{
+	int	n;
+
+	n = 0;
+	while (new_var[n])
+	{
+		printf("testing	%c\n", new_var[n]);
+		if (new_var[n] == '+')
+			return (TRUE);
+		if (new_var[n] == '=')
+			return (FALSE);
+		n++;
+	}
+	return (FALSE);
+}
+
+int	envinsert(t_ms *ms, char *new_var, int env_varl)
+{
+	int	n;
+
+	printf("envinsert called\n");
+	n = 0;
+	while (ms->envp[env_varl][n])
+		n++;
+	if (ft_strinsert(ms->envp + env_varl, new_var, n))
+		return (1);
+	return (0);
+}
+
 
 /**
  * export a new var into ms_env. Carefull, it doesn't free new var.
@@ -59,13 +105,19 @@ int	ms_exportvar(char *new_var, t_ms *ms)
 		return (1);
 	}
 	env_varl = ms_getenv_varl(new_var, ms);
-	if (env_varl >= 0)
-	{
-		free(ms->envp[env_varl]);
-		ms->envp[env_varl] = ft_strdup(new_var);
-	}
-	else
+	if (env_varl == -1)
 		env_addvar(new_var, ms);
+	else
+	{
+		if (is_envinsert(new_var))
+			envinsert(ms, new_var, env_varl);
+		else
+		{
+			printf("envreplace called\n");			
+			free(ms->envp[env_varl]);
+			ms->envp[env_varl] = ft_strdup(new_var);
+		}		
+	}
 	return (0);
 }
 
